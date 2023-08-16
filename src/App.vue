e84be50b886ef804ed40574a959e090a4942b8fe
why <375989120@qq.com>
2023-08-16 19:29:07 +0800

    login-why

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/login">登录</router-link>
-    <router-link to="/main">首页</router-link>
-  </nav>
-  <router-view />
+  <div class="app">
+    <router-view />
+  </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default defineComponent({
+  name: 'app',
+  props: {
+    name: {
+      type: String
     }
   }
+})
+</script>
+
+<style lang="less">
+.app {
+  height: 100%;
 }
 </style>
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
-    <el-button>登录</el-button>
-    Login
+  <div class="login">
+    <login-panel />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import LoginPanel from './cpns/login-panel.vue'
+
 export default defineComponent({
-  setup() {
-    return {}
+  components: {
+    LoginPanel
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background: url('../../assets/img/login-bg.svg');
+}
+</style>
